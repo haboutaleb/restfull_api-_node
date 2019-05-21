68f3b385abab2330216eec68452b2fa4f8ca4a92
hanyaboutaleb <henooo2012@gmail.com>
2019-05-21 15:49:46 +0200

    noderestfulapi

---
+++ server.js
@@ -0,0 +1,29 @@
+require('./models/db');
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const bodyparser = require('body-parser');
+const employeeController = require('./controllers/employeeController');
+
+var app = express();
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
+app.use((req , res, next)=>{
+    console.log("hany middle ware")
+    next();
+});
+// app.use((req, res, next)=>{
+//     res.send('<h1> hany</h1>');
+//     next();
+// });
+app.use(bodyparser.json());
+app.set('views', path.join(__dirname, '/views/'));
+app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
+app.set('view engine', 'hbs');
+
+app.listen(3000, () => {
+    console.log('Express server started at port : 3000');
+});
+
+app.use('/employee', employeeController);
